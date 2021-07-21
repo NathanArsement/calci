83fb22d845fac2cc0b872e87a6400a5d97910694
Nathan Arsement <nathanarsement@gmail.com>
2021-07-21 12:33:24 +0530

    Add files via upload

---
+++ chemistry/element.js
@@ -0,0 +1,165 @@
+var atomicNumber, atomicMass, atomicWeight, valency, valenceElectron, period, group, i;
+var tabcontent, tablinks;
+
+function nextPage(a, b, c, d, e, f, g, tab1, tabD) {
+  atomicNumber = a;
+  atomicMass = b;
+  valency = c;
+  atomicWeight = d;
+  valenceElectron = e;
+  period = f;
+  group = g;
+  //window.open(tab1 );
+  switch (atomicNumber) {
+    case 1:
+      document.getElementById("body").innerHTML = "<h3>Hydrogen</h3><br><table border='1'><tr><td>Atomic Number</td><td>1</td></tr><tr><td>Atomic Mass</td><td>1.008</td></tr></table><p onclick='location.reload()' style='position:absolute;top:1%;right:5%'>X</p>";
+      break;
+    case 2:
+      document.getElementById("body").innerHTML = "<h3>Hydrogen</h3><br><table border='1'><tr><td>Atomic Number</td><td>2</td></tr><tr><td>Atomic Mass</td><td>4.002</td></tr></table><p onclick='location.reload()' style='position:absolute;top:1%;right:5%'>X</p>";
+      break;
+    case 2:
+      document.getElementById("body").innerHTML = "<h3>Hydrogen</h3><br><table border='1'><tr><td>Atomic Number</td><td>2</td></tr><tr><td>Atomic Mass</td><td>4.002</td></tr></table><p onclick='location.reload()' style='position:absolute;top:1%;right:5%'>X</p>";
+      break;
+    case 2:
+      document.getElementById("body").innerHTML = "<h3>Hydrogen</h3><br><table border='1'><tr><td>Atomic Number</td><td>2</td></tr><tr><td>Atomic Mass</td><td>4.002</td></tr></table><p onclick='location.reload()' style='position:absolute;top:1%;right:5%'>X</p>";
+      break;
+    case 2:
+      document.getElementById("body").innerHTML = "<h3>Hydrogen</h3><br><table border='1'><tr><td>Atomic Number</td><td>2</td></tr><tr><td>Atomic Mass</td><td>4.002</td></tr></table><p onclick='location.reload()' style='position:absolute;top:1%;right:5%'>X</p>";
+      break;
+    case 2:
+      document.getElementById("body").innerHTML = "<h3>Hydrogen</h3><br><table border='1'><tr><td>Atomic Number</td><td>2</td></tr><tr><td>Atomic Mass</td><td>4.002</td></tr></table><p onclick='location.reload()' style='position:absolute;top:1%;right:5%'>X</p>";
+      break;
+    case 2:
+      document.getElementById("body").innerHTML = "<h3>Hydrogen</h3><br><table border='1'><tr><td>Atomic Number</td><td>2</td></tr><tr><td>Atomic Mass</td><td>4.002</td></tr></table><p onclick='location.reload()' style='position:absolute;top:1%;right:5%'>X</p>";
+      break;
+    case 2:
+      document.getElementById("body").innerHTML = "<h3>Hydrogen</h3><br><table border='1'><tr><td>Atomic Number</td><td>2</td></tr><tr><td>Atomic Mass</td><td>4.002</td></tr></table><p onclick='location.reload()' style='position:absolute;top:1%;right:5%'>X</p>";
+      break;
+    case 2:
+      document.getElementById("body").innerHTML = "<h3>Hydrogen</h3><br><table border='1'><tr><td>Atomic Number</td><td>2</td></tr><tr><td>Atomic Mass</td><td>4.002</td></tr></table><p onclick='location.reload()' style='position:absolute;top:1%;right:5%'>X</p>";
+      break;
+    case 2:
+      document.getElementById("body").innerHTML = "<h3>Hydrogen</h3><br><table border='1'><tr><td>Atomic Number</td><td>2</td></tr><tr><td>Atomic Mass</td><td>4.002</td></tr></table><p onclick='location.reload()' style='position:absolute;top:1%;right:5%'>X</p>";
+      break;
+    case 2:
+      document.getElementById("body").innerHTML = "<h3>Hydrogen</h3><br><table border='1'><tr><td>Atomic Number</td><td>2</td></tr><tr><td>Atomic Mass</td><td>4.002</td></tr></table><p onclick='location.reload()' style='position:absolute;top:1%;right:5%'>X</p>";
+      break;
+    case 2:
+      document.getElementById("body").innerHTML = "<h3>Hydrogen</h3><br><table border='1'><tr><td>Atomic Number</td><td>2</td></tr><tr><td>Atomic Mass</td><td>4.002</td></tr></table><p onclick='location.reload()' style='position:absolute;top:1%;right:5%'>X</p>";
+      break;
+    case 2:
+      document.getElementById("body").innerHTML = "<h3>Hydrogen</h3><br><table border='1'><tr><td>Atomic Number</td><td>2</td></tr><tr><td>Atomic Mass</td><td>4.002</td></tr></table><p onclick='location.reload()' style='position:absolute;top:1%;right:5%'>X</p>";
+      break;
+    case 2:
+      document.getElementById("body").innerHTML = "<h3>Hydrogen</h3><br><table border='1'><tr><td>Atomic Number</td><td>2</td></tr><tr><td>Atomic Mass</td><td>4.002</td></tr></table><p onclick='location.reload()' style='position:absolute;top:1%;right:5%'>X</p>";
+      break;
+    case 2:
+      document.getElementById("body").innerHTML = "<h3>Hydrogen</h3><br><table border='1'><tr><td>Atomic Number</td><td>2</td></tr><tr><td>Atomic Mass</td><td>4.002</td></tr></table><p onclick='location.reload()' style='position:absolute;top:1%;right:5%'>X</p>";
+      break;
+    case 2:
+      document.getElementById("body").innerHTML = "<h3>Hydrogen</h3><br><table border='1'><tr><td>Atomic Number</td><td>2</td></tr><tr><td>Atomic Mass</td><td>4.002</td></tr></table><p onclick='location.reload()' style='position:absolute;top:1%;right:5%'>X</p>";
+      break;
+    case 2:
+      document.getElementById("body").innerHTML = "<h3>Hydrogen</h3><br><table border='1'><tr><td>Atomic Number</td><td>2</td></tr><tr><td>Atomic Mass</td><td>4.002</td></tr></table><p onclick='location.reload()' style='position:absolute;top:1%;right:5%'>X</p>";
+      break;
+    case 2:
+      document.getElementById("body").innerHTML = "<h3>Hydrogen</h3><br><table border='1'><tr><td>Atomic Number</td><td>2</td></tr><tr><td>Atomic Mass</td><td>4.002</td></tr></table><p onclick='location.reload()' style='position:absolute;top:1%;right:5%'>X</p>";
+      break;
+    case 2:
+      document.getElementById("body").innerHTML = "<h3>Hydrogen</h3><br><table border='1'><tr><td>Atomic Number</td><td>2</td></tr><tr><td>Atomic Mass</td><td>4.002</td></tr></table><p onclick='location.reload()' style='position:absolute;top:1%;right:5%'>X</p>";
+      break;
+    case 2:
+      document.getElementById("body").innerHTML = "<h3>Hydrogen</h3><br><table border='1'><tr><td>Atomic Number</td><td>2</td></tr><tr><td>Atomic Mass</td><td>4.002</td></tr></table><p onclick='location.reload()' style='position:absolute;top:1%;right:5%'>X</p>";
+      break;
+    case 2:
+      document.getElementById("body").innerHTML = "<h3>Hydrogen</h3><br><table border='1'><tr><td>Atomic Number</td><td>2</td></tr><tr><td>Atomic Mass</td><td>4.002</td></tr></table><p onclick='location.reload()' style='position:absolute;top:1%;right:5%'>X</p>";
+      break;
+    case 2:
+      document.getElementById("body").innerHTML = "<h3>Hydrogen</h3><br><table border='1'><tr><td>Atomic Number</td><td>2</td></tr><tr><td>Atomic Mass</td><td>4.002</td></tr></table><p onclick='location.reload()' style='position:absolute;top:1%;right:5%'>X</p>";
+      break;
+    case 2:
+      document.getElementById("body").innerHTML = "<h3>Hydrogen</h3><br><table border='1'><tr><td>Atomic Number</td><td>2</td></tr><tr><td>Atomic Mass</td><td>4.002</td></tr></table><p onclick='location.reload()' style='position:absolute;top:1%;right:5%'>X</p>";
+      break;
+    case 2:
+      document.getElementById("body").innerHTML = "<h3>Hydrogen</h3><br><table border='1'><tr><td>Atomic Number</td><td>2</td></tr><tr><td>Atomic Mass</td><td>4.002</td></tr></table><p onclick='location.reload()' style='position:absolute;top:1%;right:5%'>X</p>";
+      break;
+    case 2:
+      document.getElementById("body").innerHTML = "<h3>Hydrogen</h3><br><table border='1'><tr><td>Atomic Number</td><td>2</td></tr><tr><td>Atomic Mass</td><td>4.002</td></tr></table><p onclick='location.reload()' style='position:absolute;top:1%;right:5%'>X</p>";
+      break;
+    case 2:
+      document.getElementById("body").innerHTML = "<h3>Hydrogen</h3><br><table border='1'><tr><td>Atomic Number</td><td>2</td></tr><tr><td>Atomic Mass</td><td>4.002</td></tr></table><p onclick='location.reload()' style='position:absolute;top:1%;right:5%'>X</p>";
+      break;
+    case 2:
+      document.getElementById("body").innerHTML = "<h3>Hydrogen</h3><br><table border='1'><tr><td>Atomic Number</td><td>2</td></tr><tr><td>Atomic Mass</td><td>4.002</td></tr></table><p onclick='location.reload()' style='position:absolute;top:1%;right:5%'>X</p>";
+      break;
+    case 2:
+      document.getElementById("body").innerHTML = "<h3>Hydrogen</h3><br><table border='1'><tr><td>Atomic Number</td><td>2</td></tr><tr><td>Atomic Mass</td><td>4.002</td></tr></table><p onclick='location.reload()' style='position:absolute;top:1%;right:5%'>X</p>";
+      break;
+    case 2:
+      document.getElementById("body").innerHTML = "<h3>Hydrogen</h3><br><table border='1'><tr><td>Atomic Number</td><td>2</td></tr><tr><td>Atomic Mass</td><td>4.002</td></tr></table><p onclick='location.reload()' style='position:absolute;top:1%;right:5%'>X</p>";
+      break;
+    case 2:
+      document.getElementById("body").innerHTML = "<h3>Hydrogen</h3><br><table border='1'><tr><td>Atomic Number</td><td>2</td></tr><tr><td>Atomic Mass</td><td>4.002</td></tr></table><p onclick='location.reload()' style='position:absolute;top:1%;right:5%'>X</p>";
+      break;
+    case 2:
+      document.getElementById("body").innerHTML = "<h3>Hydrogen</h3><br><table border='1'><tr><td>Atomic Number</td><td>2</td></tr><tr><td>Atomic Mass</td><td>4.002</td></tr></table><p onclick='location.reload()' style='position:absolute;top:1%;right:5%'>X</p>";
+      break;
+    case 2:
+      document.getElementById("body").innerHTML = "<h3>Hydrogen</h3><br><table border='1'><tr><td>Atomic Number</td><td>2</td></tr><tr><td>Atomic Mass</td><td>4.002</td></tr></table><p onclick='location.reload()' style='position:absolute;top:1%;right:5%'>X</p>";
+      break;
+    case 2:
+      document.getElementById("body").innerHTML = "<h3>Hydrogen</h3><br><table border='1'><tr><td>Atomic Number</td><td>2</td></tr><tr><td>Atomic Mass</td><td>4.002</td></tr></table><p onclick='location.reload()' style='position:absolute;top:1%;right:5%'>X</p>";
+      break;
+    case 2:
+      document.getElementById("body").innerHTML = "<h3>Hydrogen</h3><br><table border='1'><tr><td>Atomic Number</td><td>2</td></tr><tr><td>Atomic Mass</td><td>4.002</td></tr></table><p onclick='location.reload()' style='position:absolute;top:1%;right:5%'>X</p>";
+      break;
+    case 2:
+      document.getElementById("body").innerHTML = "<h3>Hydrogen</h3><br><table border='1'><tr><td>Atomic Number</td><td>2</td></tr><tr><td>Atomic Mass</td><td>4.002</td></tr></table><p onclick='location.reload()' style='position:absolute;top:1%;right:5%'>X</p>";
+      break;
+    case 2:
+      document.getElementById("body").innerHTML = "<h3>Hydrogen</h3><br><table border='1'><tr><td>Atomic Number</td><td>2</td></tr><tr><td>Atomic Mass</td><td>4.002</td></tr></table><p onclick='location.reload()' style='position:absolute;top:1%;right:5%'>X</p>";
+      break;
+    case 2:
+      document.getElementById("body").innerHTML = "<h3>Hydrogen</h3><br><table border='1'><tr><td>Atomic Number</td><td>2</td></tr><tr><td>Atomic Mass</td><td>4.002</td></tr></table><p onclick='location.reload()' style='position:absolute;top:1%;right:5%'>X</p>";
+      break;
+    case 2:
+      document.getElementById("body").innerHTML = "<h3>Hydrogen</h3><br><table border='1'><tr><td>Atomic Number</td><td>2</td></tr><tr><td>Atomic Mass</td><td>4.002</td></tr></table><p onclick='location.reload()' style='position:absolute;top:1%;right:5%'>X</p>";
+      break;
+    case 2:
+      document.getElementById("body").innerHTML = "<h3>Hydrogen</h3><br><table border='1'><tr><td>Atomic Number</td><td>2</td></tr><tr><td>Atomic Mass</td><td>4.002</td></tr></table><p onclick='location.reload()' style='position:absolute;top:1%;right:5%'>X</p>";
+      break;
+    case 2:
+      document.getElementById("body").innerHTML = "<h3>Hydrogen</h3><br><table border='1'><tr><td>Atomic Number</td><td>2</td></tr><tr><td>Atomic Mass</td><td>4.002</td></tr></table><p onclick='location.reload()' style='position:absolute;top:1%;right:5%'>X</p>";
+      break;
+    case 2:
+      document.getElementById("body").innerHTML = "<h3>Hydrogen</h3><br><table border='1'><tr><td>Atomic Number</td><td>2</td></tr><tr><td>Atomic Mass</td><td>4.002</td></tr></table><p onclick='location.reload()' style='position:absolute;top:1%;right:5%'>X</p>";
+      break;
+    case 2:
+      document.getElementById("body").innerHTML = "<h3>Hydrogen</h3><br><table border='1'><tr><td>Atomic Number</td><td>2</td></tr><tr><td>Atomic Mass</td><td>4.002</td></tr></table><p onclick='location.reload()' style='position:absolute;top:1%;right:5%'>X</p>";
+      break;
+    case 2:
+      document.getElementById("body").innerHTML = "<h3>Hydrogen</h3><br><table border='1'><tr><td>Atomic Number</td><td>2</td></tr><tr><td>Atomic Mass</td><td>4.002</td></tr></table><p onclick='location.reload()' style='position:absolute;top:1%;right:5%'>X</p>";
+      break;
+    case 2:
+      document.getElementById("body").innerHTML = "<h3>Hydrogen</h3><br><table border='1'><tr><td>Atomic Number</td><td>2</td></tr><tr><td>Atomic Mass</td><td>4.002</td></tr></table><p onclick='location.reload()' style='position:absolute;top:1%;right:5%'>X</p>";
+      break;
+    case 2:
+      document.getElementById("body").innerHTML = "<h3>Hydrogen</h3><br><table border='1'><tr><td>Atomic Number</td><td>2</td></tr><tr><td>Atomic Mass</td><td>4.002</td></tr></table><p onclick='location.reload()' style='position:absolute;top:1%;right:5%'>X</p>";
+      break;
+    case 2:
+      document.getElementById("body").innerHTML = "<h3>Hydrogen</h3><br><table border='1'><tr><td>Atomic Number</td><td>2</td></tr><tr><td>Atomic Mass</td><td>4.002</td></tr></table><p onclick='location.reload()' style='position:absolute;top:1%;right:5%'>X</p>";
+      break;
+    case 2:
+      document.getElementById("body").innerHTML = "<h3>Hydrogen</h3><br><table border='1'><tr><td>Atomic Number</td><td>2</td></tr><tr><td>Atomic Mass</td><td>4.002</td></tr></table><p onclick='location.reload()' style='position:absolute;top:1%;right:5%'>X</p>";
+      break;
+    case 2:
+      document.getElementById("body").innerHTML = "<h3>Hydrogen</h3><br><table border='1'><tr><td>Atomic Number</td><td>2</td></tr><tr><td>Atomic Mass</td><td>4.002</td></tr></table><p onclick='location.reload()' style='position:absolute;top:1%;right:5%'>X</p>";
+      break;
+
+
+  }
+
+}
+
+//document.getElementById("number").innerHTML=atomicNumber;
+  //  document.getElementById("weight").innerHTML=atomicWeight;
